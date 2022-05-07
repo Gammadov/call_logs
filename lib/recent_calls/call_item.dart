b45d726ad93952f50b45ed01a0361f2deaa7aaba
Sha.gam <gammadov@gmail.com>
2022-05-07 22:36:52 +0300

    les 83 call_item remove unnecessary padding

--- lib/recent_calls/call_item.dart
+++ lib/recent_calls/call_item.dart
@@ -13,38 +13,35 @@ class CallCard extends StatefulWidget {
 class _CallCardState extends State<CallCard> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(right: 0),
-      child: Row(
-        children: [
-          const Padding(
-            padding: EdgeInsets.only(left: 14, top: 15, right: 14, bottom: 31),
-            child: Icon(
-              Icons.phone_forwarded,
-              color: AppColor.tertiary,
-              size: 14,
-            ),
+    return Row(
+      children: [
+        const Padding(
+          padding: EdgeInsets.only(left: 14, top: 15, right: 14, bottom: 31),
+          child: Icon(
+            Icons.phone_forwarded,
+            color: AppColor.tertiary,
+            size: 14,
           ),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('+0 (000) 000-00-00', style: AppTextStyle.bold17()),
-                Text('Доп. информация', style: AppTextStyle.regular15()),
-              ],
-            ),
+        ),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('+0 (000) 000-00-00', style: AppTextStyle.bold17()),
+              Text('Доп. информация', style: AppTextStyle.regular15()),
+            ],
           ),
-          Text('Дата', style: AppTextStyle.regular15()),
-          IconButton(
-              padding: const EdgeInsets.only(left: 8, right: 21),
-              onPressed: () => Navigator.pushNamed(context, '/info'),
-              icon: const Icon(
-                Icons.info_outlined,
-                color: AppColor.link,
-                size: 22,
-              )),
-        ],
-      ),
+        ),
+        Text('Дата', style: AppTextStyle.regular15()),
+        IconButton(
+            padding: const EdgeInsets.only(left: 8, right: 21),
+            onPressed: () => Navigator.pushNamed(context, '/info'),
+            icon: const Icon(
+              Icons.info_outlined,
+              color: AppColor.link,
+              size: 22,
+            )),
+      ],
     );
   }
 }
